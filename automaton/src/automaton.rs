264531835075d0009de56f489e7a6c943b253ad6
hetakan <hetakan@gmail.com>
2019-05-04 02:32:28 +0900

    Use `Self`

--- automaton/src/automaton.rs
+++ automaton/src/automaton.rs
@@ -5,13 +5,11 @@ pub type NFA<'a> = Graph<'a>;
 pub struct Node {
     v: i32
 }
-
 impl Node {
-    pub fn spawn(n: i32) -> Vec<Node> {
+    pub fn spawn(n: i32) -> Vec<Self> {
         (0..n).fold(vec![], |mut acc, i| { acc.push(Node{v: i}); acc })
     }
 }
-
 impl PartialEq for Node {
     fn eq(&self, other: &Node) -> bool {
         self.v == other.v
@@ -26,26 +24,26 @@ pub struct Edge {
     epsilon: bool,
 }
 impl Edge {
-    fn spawn_inner(s1: State, s2: State, name: &str, epsilon: bool) -> Edge {
-        Edge {
+    fn spawn_inner(s1: State, s2: State, name: &str, epsilon: bool) -> Self {
+        Self {
             head: s1,
             tail: s2,
             label: String::from(name),
             epsilon: epsilon,
         }
     }
-    pub fn spawn(s1: State, s2: State, name: &str) -> Edge {
+    pub fn spawn(s1: State, s2: State, name: &str) -> Self {
         if s1.v > s2.v {
-            Edge::spawn_inner(s2, s1, name, false)
+            Self::spawn_inner(s2, s1, name, false)
         } else {
-            Edge::spawn_inner(s1, s2, name, false)
+            Self::spawn_inner(s1, s2, name, false)
         }
     }
-    pub fn spawn_loop(s: State, name: &str) -> Edge {
-        Edge::spawn_inner(s, s, name, false)
+    pub fn spawn_loop(s: State, name: &str) -> Self {
+        Self::spawn_inner(s, s, name, false)
     } 
-    pub fn spawn_epsilon(s1: State, s2: State) -> Edge {
-        Edge::spawn_inner(s1, s2, "epsilon", true)
+    pub fn spawn_epsilon(s1: State, s2: State) -> Self {
+        Self::spawn_inner(s1, s2, "epsilon", true)
     }
     pub fn is_epsilon(&self) -> bool {
         self.epsilon
@@ -62,9 +60,7 @@ pub struct Graph<'a> {
 }
 impl<'a> Graph<'a> {
     pub fn make(states: &'a Vec<State>, edges: &'a Vec<Edge>) -> Graph<'a> {
-        Graph {
-            states: states, edges: edges,
-        }
+        Self { states: states, edges: edges }
     }
     pub fn epsilon_nearest_closure(&self, s: State) -> Vec<State> {
         self.edges
