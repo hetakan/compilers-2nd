41ca3f69f8ff340cbecc09d88fcfb8cae701d448
hetakan <hetakan@gmail.com>
2019-05-04 02:24:47 +0900

    Arrange edges

--- automaton/src/main.rs
+++ automaton/src/main.rs
@@ -3,15 +3,14 @@ use crate::automaton::*;
 
 fn main() {
     let states = State::spawn(4);
-    let mut edges: Vec<Edge> = vec![];
-
     // (a|b)*abb
-    edges.push(Edge::spawn_loop(states[0], "a"));
-    edges.push(Edge::spawn_loop(states[0], "b"));
-    edges.push(Edge::spawn(states[0], states[1], "a"));
-    edges.push(Edge::spawn(states[1], states[2], "b"));
-    edges.push(Edge::spawn(states[2], states[3], "b"));
-
+    let edges = vec![
+        Edge::spawn_loop(states[0], "a"),
+        Edge::spawn_loop(states[0], "b"),
+        Edge::spawn(states[0], states[1], "a"),
+        Edge::spawn(states[1], states[2], "b"),
+        Edge::spawn(states[2], states[3], "b"),
+    ];
     let nfa = NFA::make(&states, &edges);
 
     println!("graph: {:#?}", nfa);
